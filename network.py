043d892c6bf26e2df5c8277d639d4b5de2c9784a
Koki Ryu <liukoki@gmail.com>
2020-04-29 04:15:09 +0900

    3 Layers LSTM

--- network.py
+++ network.py
@@ -7,8 +7,8 @@ LSTM_OUT_DIM = 512
 class VQANN(nn.Module):
     def __init__(self, image_dim, out_dim):
         super(VQANN, self).__init__()
-        self.lstm = nn.LSTM(EMBEDDING_DIM, LSTM_OUT_DIM, 2)
-        self.fclstm = nn.Linear(LSTM_OUT_DIM * 4, LSTM_OUT_DIM * 2)
+        self.lstm = nn.LSTM(EMBEDDING_DIM, LSTM_OUT_DIM, 3)
+        self.fclstm = nn.Linear(LSTM_OUT_DIM * 6, LSTM_OUT_DIM * 2)
         self.fcim = nn.Linear(image_dim, LSTM_OUT_DIM * 2)
         self.fc1 = nn.Linear(LSTM_OUT_DIM * 2, out_dim)
         self.fc2 = nn.Linear(out_dim, out_dim)
@@ -17,7 +17,7 @@ class VQANN(nn.Module):
     def forward(self, words, image, word_lengths):
         x = nn.utils.rnn.pack_padded_sequence(words, word_lengths, batch_first=True, enforce_sorted=False)
         _, hidden = self.lstm(x)
-        lstm_out = torch.cat((hidden[0][0], hidden[0][1], hidden[1][0], hidden[1][1]), 1)
+        lstm_out = torch.cat((hidden[0][0], hidden[0][1], hidden[0][2], hidden[1][0], hidden[1][1], hidden[1][2]), 1)
         lstm_out = self.fclstm(lstm_out)
         im_out = F.relu(self.fcim(image))
         im_lstm = lstm_out + im_out
