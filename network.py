11fa6da43f9c2e3099a4fe86989dbc25227530f1
Koki Ryu <liukoki@gmail.com>
2020-04-28 17:51:25 +0900

    Use 4 hidden parameters

--- network.py
+++ network.py
@@ -3,18 +3,20 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 EMBEDDING_DIM = 300
-LSTM_OUT_DIM = 1024
+LSTM_OUT_DIM = 512
 class VQANN(nn.Module):
     def __init__(self, image_dim, out_dim):
         super(VQANN, self).__init__()
         self.lstm = nn.LSTM(EMBEDDING_DIM, LSTM_OUT_DIM, 2)
-        self.fcim = nn.Linear(image_dim, LSTM_OUT_DIM )
-        self.fc1 = nn.Linear(LSTM_OUT_DIM, out_dim)
+        self.fclstm = nn.Linear(LSTM_OUT_DIM * 4, LSTM_OUT_DIM * 2)
+        self.fcim = nn.Linear(image_dim, LSTM_OUT_DIM * 2)
+        self.fc1 = nn.Linear(LSTM_OUT_DIM * 2, out_dim)
 
     def forward(self, words, image, word_lengths):
         x = nn.utils.rnn.pack_padded_sequence(words, word_lengths, batch_first=True, enforce_sorted=False)
         _, hidden = self.lstm(x)
-        lstm_out = hidden[0][1].view(-1, LSTM_OUT_DIM)
+        lstm_out = torch.cat((hidden[0][0], hidden[0][1], hidden[1][0], hidden[1][1]), 1)
+        lstm_out = self.fclstm(lstm_out)
         im_out = F.relu(self.fcim(image))
         im_lstm = lstm_out + im_out
         out = self.fc1(im_lstm)
